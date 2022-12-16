fd0b315a78202263df3ed00504951849b344ecc9
Aldair Avelino <aldair03avelino@gmail.com>
2022-12-16 10:19:57 +0100

    Exercicio para trocar valores de duas variaveis - AA

---
+++ 01/primeiro-exercicio.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+main(){
+	int a, b, atribuicaoA, atribuicaoB;
+	a=10;
+	b=20;
+	atribuicaoA=b;
+	atribuicaoB=a;
+	printf("Valor de A: %d\nValor de B: %d\n",atribuicaoA, atribuicaoB);
+	system("pause");
+}
